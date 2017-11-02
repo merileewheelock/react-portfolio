22bec19a0bf94fc64b5ffadf8778966eee54f7dc
Merilee Wheelock <eelirem@gmail.com>
2017-11-02 16:16:26 -0700

    update home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,9 @@ class Home extends Component{
 	render() {
 		return(
 			<div className="page">
-				<h1>Home</h1>
+				<h1>Hello,</h1>
+				<h1>I'm Merilee.</h1>
+				<h4>Full-Stack Web Developer | Sunnyvale, CA</h4>
 			</div>
 		)
 	}
