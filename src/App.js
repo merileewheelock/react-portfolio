f51e19ed9e8bf3255aef61bf669718d0e8aa629a
Merilee Wheelock <eelirem@gmail.com>
2017-11-03 13:45:55 -0700

    create about section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
 import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
 	render() {
@@ -12,6 +13,7 @@ class App extends Component {
 					<NavBar />
 					<div>
 						<Route exact path="/" component={Home} />
+						<Route exact path="/about" component={About} />
 					</div>
 				</div>
 			</Router>
